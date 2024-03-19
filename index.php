692241acb2fcc76cd169e143ce4e00764618d544
George Karalis <karalisg@gmail.com>
2024-03-19 11:15:50 +0200

    Extract a PHP database class

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+// Connect to DB and execute a querie
+class Database {
+
+}
 
 // Conect to MySQL database
 // 1. Initialise PDO
-class Person {
-  public
+$dsn = "mysql:host=127.0.0.1;dbname=track_expense;port=3306;user=root;password=QAZwsx123;charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+// prepare new querie
+$statement = $pdo->prepare("select * from users");
+$statement->execute();
+
+$users = $statement->fetchAll(PDO::FETCH_ASSOC);
+//dd($users);
+
+foreach ($users as $user) {
+  echo "<ul>" . $user['id'] . " " .  $user['name'] . "</ul>";
 }
