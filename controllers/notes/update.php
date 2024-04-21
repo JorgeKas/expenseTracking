78b54aa1f3cc904feea143990c40b11f85ed704e
George <karalisg@gmail.com>
2024-04-22 01:34:21 +0300

    Update w/ patch request

---
+++ controllers/notes/update.php
@@ -0,0 +1,26 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 1;
+
+// find the corresponding note in the database
+$note = $db->query('select * from notes where id = :id', [
+  'id' => $_POST['id']
+])->findOrFail();
+
+// authorize that the user is the owner of the note
+authorize($note['user_id'] === $currentUserId);
+
+// validate the body of the note
+$errors = [];
+
+if (!Validator::string($_POST['body'], 1, 10)) {
+  $errors['body'] = 'A body of no more than 10 chars is required';
+}
+
+// if no errors are found, update the note in the database table
